3fc702664ee0e8fd016b8192b51784bfad5dbba1
Ekaterina Pavlova <sone4ka1414@gmail.com>
2023-05-03 21:37:28 +0300

    preliminary version

---
+++ src/main/java/org/netology/planner/Epic.java
@@ -0,0 +1,25 @@
+package org.netology.planner;
+
+public class Epic extends Task {
+    String[] subtasks = new String[0];
+
+    public Epic(int id, String[] subtasks) {
+        super(id);
+        this.subtasks = subtasks;
+    }
+
+    public String[] getSubtasks() {
+        return subtasks;
+    }
+
+    @Override
+    public boolean matches(String query) {
+        for (String s : subtasks) {
+            if (s.contains(query)) {
+                return true;
+            }
+        } return false;
+    }
+}
+
+
--- src/main/java/org/netology/planner/Main.java
+++ src/main/java/org/netology/planner/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.netology.planner;
 
 public class Main {
     public static void main(String[] args) {
