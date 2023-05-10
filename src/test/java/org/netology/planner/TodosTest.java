eae6988b260ab3a51cb2bb984d834e94ae951bb0
Ekaterina Pavlova <sone4ka1414@gmail.com>
2023-05-10 13:04:51 +0300

    tests and a class added

--- src/test/java/org/netology/planner/TodosTest.java
+++ src/test/java/org/netology/planner/TodosTest.java
@@ -30,140 +30,7 @@ public class TodosTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 
-    @Test
-    public void shouldFindMatchingTasksSimpleTask() {
-        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
 
-        simpleTask.matches("родителям");
-
-        boolean expected = true;
-        boolean actual = simpleTask.matches("родителям");
-
-        Assertions.assertEquals(expected, actual);
-    }
-    @Test
-    public void shouldFindMatchingTasksEpicInOneSubtask() {
-        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
-        Epic epic = new Epic(2, subtasks);
-
-        epic.matches("Хлеб");
-
-        boolean expected = true;
-        boolean actual = epic.matches("Хлеб");
-
-        Assertions.assertEquals(expected, actual);
-    }
-    @Test
-    public void shouldNotFindDifferentTasksEpicInOneSubtask() {
-        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
-        Epic epic = new Epic(2, subtasks);
-
-        epic.matches("Сыр");
-
-        boolean expected = false;
-        boolean actual = epic.matches("Сыр");
-
-        Assertions.assertEquals(expected, actual);
-    }
-    @Test
-    public void shouldFindMatchingTasksEpicInTwoSubtasks() {
-        String[] subtasks = { "Сыр свежий", "Яйца", "Хлеб свежий" };
-        Epic epic = new Epic(2, subtasks);
-
-        epic.matches("свежий");
-
-        boolean expected = true;
-        boolean actual = epic.matches("свежий");
-
-        Assertions.assertEquals(expected, actual);
-    }
-    @Test
-    public void shouldFindMatchingTasksMeetingIfTopic() {
-        Meeting meeting = new Meeting(
-                555,
-                "Выкатка 3й версии приложения",
-                "Приложение НетоБанка",
-                "Во вторник после обеда"
-        );
-
-        meeting.matches("версии");
-
-        boolean expected = true;
-        boolean actual = meeting.matches("версии");
-
-        Assertions.assertEquals(expected, actual);
-    }
-    @Test
-    public void shouldNotFindDifferentTasksMeetingIfTopic() {
-        Meeting meeting = new Meeting(
-                555,
-                "Выкатка 3й версии приложения",
-                "Приложение НетоБанка",
-                "Во вторник после обеда"
-        );
-
-        meeting.matches("сдача");
-
-        boolean expected = false;
-        boolean actual = meeting.matches("сдача");
-
-        Assertions.assertEquals(expected, actual);
-    }
-    @Test
-    public void shouldFindMatchingTasksMeetingIfProject() {
-        Meeting meeting = new Meeting(
-                555,
-                "Выкатка 3й версии приложения",
-                "Приложение НетоБанка",
-                "Во вторник после обеда"
-        );
-
-        meeting.matches("НетоБанка");
-
-        boolean expected = true;
-        boolean actual = meeting.matches("НетоБанка");
-
-        Assertions.assertEquals(expected, actual);
-    }
-    @Test
-    public void shouldNotFindDifferentTasksMeetingIfProject() {
-        Meeting meeting = new Meeting(
-                555,
-                "Выкатка 3й версии приложения",
-                "Приложение НетоБанка",
-                "Во вторник после обеда"
-        );
-
-        meeting.matches("проект");
-
-        boolean expected = false;
-        boolean actual = meeting.matches("проект");
-
-        Assertions.assertEquals(expected, actual);
-    }
-    @Test
-    public void shouldNotFindId() {
-        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
-
-        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
-        Epic epic = new Epic(5, subtasks);
-
-        Meeting meeting = new Meeting(
-                5,
-                "Выкатка 3й версии приложения",
-                "Приложение НетоБанка",
-                "Во вторник после обеда"
-        );
-
-        simpleTask.matches("5");
-        epic.matches("5");
-        meeting.matches("5");
-
-        boolean expected = false;
-        boolean actual = simpleTask.matches("5"); epic.matches("5"); meeting.matches("5");
-
-        Assertions.assertEquals(expected, actual);
-    }
     @Test
     public void shouldSearchMatchingTask() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
@@ -260,4 +127,29 @@ public class TodosTest {
         Task[] actual = todos.search("сок");
         Assertions.assertArrayEquals(expected, actual);
     }
+    @Test
+    public void equalsTest() {
+        SimpleTask simpleTask = new SimpleTask(6, "Позвонить родителям");
+
+//        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+//        Epic epic = new Epic(55, subtasks);
+//
+//        Meeting meeting = new Meeting(
+//                555,
+//                "Выкатка 3й версии приложения",
+//                "Приложение НетоБанка",
+//                "Во вторник после обеда"
+//        );
+        Task task = new Task(6);
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+
+
+        boolean expected = true;
+        boolean actual = task.equals(simpleTask.id);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
